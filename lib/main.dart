f75d49e8fec17608fc3e6ff5d3bc8ccf7bd4b71f
林発平 <10223983lin_faping@cn.tre-inc.com>
2023-05-15 14:25:30 +0900

    task3

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_study/task2/task2_app.dart';
+import 'package:flutter_study/task3/task3_app.dart';
 
 void main() {
-  runApp(const Task2App());
+  runApp(const Task3App());
 }
---
+++ lib/task3/task3_app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_study/task3/stopwatch_screen.dart';
+
+class Task3App extends StatelessWidget {
+  const Task3App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Task 3: Stopwatch',
+      theme: ThemeData(
+        useMaterial3: true,
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Task 2: Stopwatch'),
+        ),
+        body: const StopwatchScreen(),
+      ),
+    );
+  }
+}
