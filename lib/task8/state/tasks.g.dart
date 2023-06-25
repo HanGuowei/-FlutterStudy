d06c96bfe7fa65b3aa8605977cae7c9104f008e0
林発平 <10223983lin_faping@cn.tre-inc.com>
2023-06-25 10:49:07 +0900

    add file

---
+++ lib/task8/state/tasks.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tasks.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$tasksHash() => r'02f34b619f36b838c82a7570e63d6ea3fa30f785';
+
+/// See also [Tasks].
+@ProviderFor(Tasks)
+final tasksProvider = AutoDisposeNotifierProvider<Tasks, List<Task>>.internal(
+  Tasks.new,
+  name: r'tasksProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$tasksHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$Tasks = AutoDisposeNotifier<List<Task>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
