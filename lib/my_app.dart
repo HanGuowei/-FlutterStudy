f105cfbf43db14cfc2994c1cfcb4238fb7dfce16
10184577song_jichao <10184577song_jichao@cn.tre-inc.com>
2023-08-18 16:45:04 +0800

    debug

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_study/task1/home_page.dart';
-import 'package:flutter_study/task2/todo_list_page.dart';
-import 'package:flutter_study/task3/second_chronograph_page.dart';
 
 class MyApp extends StatelessWidget {
   @override
