0489bcf3661337f3f94804ed8157bc7666eaae33
10184577song_jichao <10184577song_jichao@cn.tre-inc.com>
2023-08-18 16:43:57 +0800

    debug

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Task',
-      home: const TodoListPage(),
+      home: const HomePage(),
     );
   }
 }
