104012d8aa75a1ed332e762b81414b3c7f2ca8aa
10184577song_jichao <10184577song_jichao@cn.tre-inc.com>
2023-08-23 08:31:51 +0800

    fix input func

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_study/task5/repository_list_page.dart';
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Task',
-      home: const RepositoryListPage(),
+      home: RepositoryListPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
